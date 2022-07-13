613ea29c86724bbc46d76a32a15932c91276e7ee
Dimickon <dimickon1@gmail.com>
2022-07-13 23:09:09 +0300

    add dynamic header and 50% adaptive

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,1 +1,32 @@
-console.log("Hello!");
+$(document).ready(function() {
+    let root =  document.documentElement
+    let header = $('#header');
+    let scrollTop = $(window).scrollTop();
+
+    $(window).on("scroll resize", function() {
+        scrollTop = $(this).scrollTop();
+
+        let headerNav = $(".header__nav");
+        let headerInner = $(".header__inner");
+
+        let headerInnerH = headerInner.innerHeight();
+        let headerNavH = headerNav.innerHeight();
+
+        if (scrollTop >= headerInnerH) {
+            headerFixed(headerInnerH);
+        }
+
+        else {
+            header.removeClass("header--fixed");
+        }
+    });
+
+    function headerFixed(headerInnerH) {
+
+        header.addClass("header--fixed");
+
+        root.style.setProperty('--header-inner', '-' + headerInnerH + 'px');
+        console.log(headerInnerH);
+    }
+
+});
