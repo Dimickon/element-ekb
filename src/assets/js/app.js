3d1ba59b34528197289e4251f21817af6f81658b
Dimickon <dimickon1@gmail.com>
2022-07-14 04:09:15 +0300

    add btn to top, fixed function header

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,32 +1,56 @@
 $(document).ready(function() {
-    let root =  document.documentElement
+    let root =  document.documentElement;
     let header = $('#header');
     let scrollTop = $(window).scrollTop();
 
+    headerFixed();
+
     $(window).on("scroll resize", function() {
+        headerFixed();
+        toTop();
+    });
+
+    $('#toTop').click(function() {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 800);
+    });
+
+    function headerFixed() {
+
         scrollTop = $(this).scrollTop();
 
         let headerNav = $(".header__nav");
         let headerInner = $(".header__inner");
 
+        let headerH = header.innerHeight();
         let headerInnerH = headerInner.innerHeight();
         let headerNavH = headerNav.innerHeight();
 
         if (scrollTop >= headerInnerH) {
-            headerFixed(headerInnerH);
+            header.addClass("header--fixed");
+
+            root.style.setProperty('--header-inner', '-' + headerInnerH + 'px');
+            root.style.setProperty('--main-top', headerNavH + 10 + 'px');
         }
 
         else {
             header.removeClass("header--fixed");
+            root.style.setProperty('--main-top', headerH + 'px');
         }
-    });
+    }
 
-    function headerFixed(headerInnerH) {
+    function toTop() {
 
-        header.addClass("header--fixed");
+        let toTopBtn = $('#toTop');
 
-        root.style.setProperty('--header-inner', '-' + headerInnerH + 'px');
-        console.log(headerInnerH);
+        if($(this).scrollTop() != 0) {
+            toTopBtn.fadeIn();
+        }
+
+        else {
+            toTopBtn.fadeOut();
+        }
     }
 
 });
